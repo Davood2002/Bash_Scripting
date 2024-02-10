07277cc49b1bcf20677f65d7db5be72974e316d4
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-02-10 23:34:09 +0000

    Update part3.sh

--- part3.sh
+++ part3.sh
@@ -1,3 +1,4 @@
+echo "--------------------------Part 3--------------------------"
 sudo umount /mnt/dev/pts
 sudo umount /mnt/dev
 sudo umount /mnt/proc
@@ -5,3 +6,4 @@ sudo umount /mnt/sys
 sudo umount /mnt || echo "Failed to unmount /mnt"
 echo "unmouting complete"
 echo "Done, everthing is OK."
+echo "--------------------------Done--------------------------"
