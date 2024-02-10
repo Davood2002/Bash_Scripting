ef818e444b4496d3ad8d448d8d7f3246fd87a6dd
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-02-10 23:33:39 +0000

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -1,6 +1,8 @@
+echo "--------------------------Part 2--------------------------"
 grub-install /dev/sda
 echo "GRUB installed successfully." 
 update-grub
 echo "GRUB updated successfully."
 echo "Exitting, now you can run ./part3.sh"
 exit
+echo "--------------------------Done--------------------------"
