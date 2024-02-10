344cef3c5babb5c7e7d6fcca83ded3cf174fef30
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-02-10 22:49:00 +0000

    part1.sh

--- chroot.sh
+++ chroot.sh
@@ -10,16 +10,3 @@ sudo mount /dev/sda1 /mnt/boot/efi
 echo "Partitions mounted successfully."
 sudo chroot /mnt
 echo "Root directory chaned successfully."
-grub-install /dev/sda
-echo "GRUB installed successfully." 
-update-grub
-echo "GRUB updated successfully."
-echo "Exitiing..."
-exit
-sudo umount /mnt/dev/pts
-sudo umount /mnt/dev
-sudo umount /mnt/proc
-sudo umount /mnt/sys
-sudo umount /mnt || echo "Failed to unmount /mnt"
-echo "unmouting complete"
-echo "Done, everthing is OK."
