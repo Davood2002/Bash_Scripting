ad93fb55c502d84c39afd806e5704e4d7c0e7b6f
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-02-10 23:43:28 +0000

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -1,4 +1,5 @@
 echo "--------------------------Part 2--------------------------"
+mount -t efivarfs none /sys/firmware/efi/efivars
 grub-install /dev/sda
 echo "GRUB installed successfully." 
 update-grub
