a84b3bd1378033b74d77a3b2963c1584c166cdbf
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-02-10 23:29:31 +0000

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -2,12 +2,5 @@ grub-install /dev/sda
 echo "GRUB installed successfully." 
 update-grub
 echo "GRUB updated successfully."
-echo "Exitiing..."
+echo "Exitting, now you can run ./part3.sh"
 exit
-sudo umount /mnt/dev/pts
-sudo umount /mnt/dev
-sudo umount /mnt/proc
-sudo umount /mnt/sys
-sudo umount /mnt || echo "Failed to unmount /mnt"
-echo "unmouting complete"
-echo "Done, everthing is OK."
