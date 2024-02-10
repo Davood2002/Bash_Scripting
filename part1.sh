8822ae2fabcfc0da4e7554f08a5eab4f47a713d8
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-02-10 23:33:03 +0000

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -1,13 +1,15 @@
 echo "Created by Davood:)"
+echo "--------------------------Part 1--------------------------"
 sudo mount /dev/sda9 /mnt
 sudo mount /dev/sda8 /mnt/boot
 sudo mount --bind /dev /mnt/dev
 sudo mount --bind /dev/pts /mnt/dev/pts
 sudo mount --bind /proc /mnt/proc
 sudo mount --bind /sys /mnt/sys
+sudo mount --bind /run /mnt/run
 sudo mount /dev/sda10 /mnt/home
 sudo mount /dev/sda1 /mnt/boot/efi
 echo "Partitions mounted successfully."
 sudo chroot /mnt
 echo "Root directory chaned successfully."
-echo "Done"
+echo "--------------------------Done--------------------------"
