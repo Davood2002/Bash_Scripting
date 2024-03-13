c1a0fbb0bbc286c33b0fbde648a056864a868a7e
Davood K.M <77622614+Davood2002@users.noreply.github.com>
2024-03-13 09:31:21 +0330

    Update part1.sh

--- part1.sh
+++ part1.sh
@@ -1,16 +1,41 @@
+#!/bin/bash
+
 echo "Created by Davood:)"
 echo "--------------------------Part 1--------------------------"
-sudo mount /dev/sda9 /mnt
-sudo mount /dev/sda8 /mnt/boot
+
+# recognize partitions
+echo "Suppose your linux root partition is on /dev/sda*. What number is *?"
+read root_par
+echo "Suppose your linux boot partition is on /dev/sda+. What number is +?"
+read boot_par
+echo "Suppose your EFI partition is on /dev/sda-. What number is -? (Usually it's 1.)"
+read efi_par
+echo "Partitions recognized sucessfully."
+
+# mount partitions
+sudo mount /dev/sda$root_par /mnt
+sudo mount /dev/sda$boot_par /mnt/boot
+sudo mount /dev/sda$efi_par /mnt/boot/efi
+echo "Partitions mounted successfully."
+
+# bind some files
 sudo mount --bind /dev /mnt/dev
 sudo mount --bind /dev/pts /mnt/dev/pts
 sudo mount --bind /proc /mnt/proc
 sudo mount --bind /sys /mnt/sys
 sudo mount --bind /run /mnt/run
-sudo mount /dev/sda10 /mnt/home
-sudo mount /dev/sda1 /mnt/boot/efi
-echo "Partitions mounted successfully."
-sduo cp part2.sh /mnt
-echo "Now please run sudo ./part2.sh"
+echo "Files were bound sucessfully."
+
+# copy files for next steps
+sudo cp part2.sh /mnt
+sudo cp part3.sh /mnt
+echo "Copying the rest of the files was done successfully."
+
+# give next step's files execute permission
+sudo chmod +x part2.sh 
+sudo chmod +x part3.sh 
+echo "Execute permissions given sucessfully."
+
+echo "Now please run this command: 'sudo ./part2.sh'"
 echo "---------------------------Done---------------------------"
 sudo chroot /mnt
